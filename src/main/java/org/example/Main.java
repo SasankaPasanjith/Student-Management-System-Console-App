c83c7048984318b8cc7ef8fcae5f2051afc5a624
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-06-19 14:30:19 +0530

    Implement View Student Function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,12 +14,17 @@ public class Main {
             System.out.println("1. Add Student");
             System.out.println("2. Display Student");
             System.out.println("Enter your choice: ");
-            System.out.println("3. Exit");
+
             int choice = input.nextInt();
             if (choice == 1) {                        //Add New Student
                 System.out.println("Enter Student ID: ");
                 input.nextLine();
                 String id = input.nextLine();
+                int index = findIndexById(arrId, id);
+                if (index != -1) {
+                    System.out.println("Student ID Already Exist. Please Add Another Student ID");
+                    continue;
+                }
                 System.out.println("Enter Student Name: ");
                 String name = input.nextLine();
                 System.out.println("Enter marks in subject 1: ");
@@ -34,6 +39,26 @@ public class Main {
                 arrSub1 = extendAddInteger(arrSub1, sub1);
                 arrSub2 = extendAddInteger(arrSub2, sub2);
                 arrSub3 = extendAddInteger(arrSub3, sub3);
+            }
+            if (choice == 2){
+                System.out.println("Enter Student ID: ");
+                input.nextLine();
+                String id = input.nextLine();
+                int index = findIndexById(arrId, id);
+                if (index != -1) {
+                    String name = arrName[index];
+                    int sub1 = arrSub1[index];
+                    int sub2 = arrSub2[index];
+                    int sub3 = arrSub3[index];
+                    System.out.println("*------------------*----------------------*");
+                    System.out.printf("| Name             | %-30s |\n",name);
+                    System.out.printf("| subject 1 Marks  | %-20d |\n",sub1);
+                    System.out.printf("| subject 2 Marks  | %-20d |\n",sub2);
+                    System.out.printf("| subject 3 Marks  | %-20d |\n",sub3);
+                    System.out.println("*------------------*----------------------*");
+                }else {
+                    System.out.println("Student not found");
+                }
             }
         }
     }
@@ -52,5 +77,14 @@ public class Main {
         }
         temp[temp.length-1] = value;
         return temp;
+    }
+
+    public static int  findIndexById(String[] arrId, String id) {
+        for (int i = 0; i < arrId.length; i++) {
+            if (arrId[i].equals(id)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
